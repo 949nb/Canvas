4a4f23803da318cdf5389b530c19a0267b746993
949nb <1661346771@qq.com>
2019-02-03 02:07:03 +0800

    1

---
+++ canvas.js
@@ -0,0 +1,49 @@
+var canvas = document.getElementById("canvas")
+console.log(canvas)
+var context = canvas.getContext("2d");
+
+var pageheight = document.documentElement.clientHeight
+var pagewidth = document.documentElement.clientWidth
+
+canvas.height = pageheight
+canvas.width = pagewidth
+
+function huayuan(x,y){
+    context.beginPath()
+    context.arc(x,y,2,0,360)
+    context.fill()
+}
+
+var t = false
+var last = {x: undefined,y: undefined}
+canvas.onmousedown = function(a){
+    t = true
+    x = a.clientX
+    y = a.clientY
+    last = {x: x,y:y}
+    huayuan(x,y)
+}
+
+canvas.onmousemove = function(a){
+    if(t){
+        x = a.clientX
+        y = a.clientY
+        n = {x:x,y:y}
+        huayuan(x,y)
+        huaxian(last.x,last.y,n.x,n.y)
+        last=n
+    }
+}
+
+canvas.onmouseup = function(a){
+    t = false;
+}
+
+function huaxian(x1,y1,x2,y2){
+  context.beginPath()
+  context.moveTo(x1,y1)
+  context.lineWidth = 4
+  context.lineTo(x2,y2)
+  context.stroke()
+  context.closePath()
+}
