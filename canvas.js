3fe2a49ded1a9e12a5a331f5fc3db90c0a6e003c
949nb <1661346771@qq.com>
2019-02-03 22:54:52 +0800

    1

--- canvas.js
+++ canvas.js
@@ -5,11 +5,11 @@ var context = canvas.getContext('2d');
 //页面尺寸
 pagesize()
 
-window.resize = function(){
+window.resize = function () {
     pagesize()
 }
 
-function pagesize(){
+function pagesize() {
     var pageheight = document.documentElement.clientHeight
     var pagewidth = document.documentElement.clientWidth
     canvas.height = pageheight
@@ -17,42 +17,61 @@ function pagesize(){
 }
 //页面尺寸
 
-function huayuan(x,y){
+var usingEraser = false
+eraser.onclick = function() {
+    usingEraser = !usingEraser
+}
+
+function huayuan(x, y) {
     context.beginPath()
-    context.arc(x,y,2,0,360)
+    context.arc(x, y, 2, 0, 360)
     context.fill()
 }
 
-var t = false
-var last = {x: undefined,y: undefined}
-canvas.onmousedown = function(a){
-    t = true
+var using = false
+var last = { x: undefined, y: undefined }
+canvas.onmousedown = function (a) {
     x = a.clientX
     y = a.clientY
-    last = {x: x,y:y}
-    huayuan(x,y)
-}
-
-canvas.onmousemove = function(a){
-    if(t){
-        x = a.clientX
-        y = a.clientY
-        n = {x:x,y:y}
-        huayuan(x,y)
-        huaxian(last.x,last.y,n.x,n.y)
-        last=n
+    using = true
+    if (usingEraser) {
+        context.clearRect(x, y, 10, 10)
+    } else {
+        last = { x: x, y: y }
+        huayuan(x, y)
     }
 }
 
-canvas.onmouseup = function(a){
-    t = false;
+
+canvas.onmousemove = function (a) {
+    x = a.clientX
+    y = a.clientY
+    if (usingEraser) {
+        if(using){
+        context.clearRect(x, y, 4, 4)
+        }else{
+            
+        }
+    } else {
+        if (using) {
+            n = { x: x, y: y }
+            huayuan(x, y)
+            huaxian(last.x, last.y, n.x, n.y)
+            last = n
+        } else {
+        }
+    }
 }
 
-function huaxian(x1,y1,x2,y2){
-  context.beginPath()
-  context.moveTo(x1,y1)
-  context.lineWidth = 4
-  context.lineTo(x2,y2)
-  context.stroke()
-  context.closePath()
+canvas.onmouseup = function (a) {
+    using = false;
+}
+
+function huaxian(x1, y1, x2, y2) {
+    context.beginPath()
+    context.moveTo(x1, y1)
+    context.lineWidth = 4
+    context.lineTo(x2, y2)
+    context.stroke()
+    context.closePath()
 }
